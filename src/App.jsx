fc2707dab2fe66713161a972cb48b499a72be196
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-06-22 11:37:19 +0200

    New app

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+import Calendario from './pages/Calendario';
+import Actividades from './pages/Actividades';
+import Personal from './pages/Sistema/Personal';
+import Maquinaria from './pages/Sistema/Maquinaria';
+import Productos from './pages/Sistema/Productos';
+import Fitosanitarios from './pages/Sistema/Fitosanitarios';
+import AñadirActividades from './pages/AñadirActividades';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  const preloader = document.getElementById('preloader');
+
+  if (preloader) {
+    setTimeout(() => {
+      preloader.style.display = 'none';
+      setLoading(false);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  const handleLogin = () => {
+    setAuthenticated(true);
+    navigate('/actividades');
+  };
+
+  return loading ? (
+    <p className="text-center text-danger">LA APLICACIÓN NO HA SIDO CARGADA</p>
+  ) : (
+    <>
+      <Routes>
+        {authenticated ? (
+          <>
+            <Route path="/actividades" element={<Actividades />} />
+            <Route path="/calendar" element={<Calendario />} />
+            <Route path="/personal" element={<Personal />} />
+            <Route path="/maquinaria" element={<Maquinaria />} />
+            <Route path="/productos" element={<Productos />} />
+            <Route path="/fitosanitarios" element={<Fitosanitarios />} />
+            <Route path="/anadiractividades" element={<AñadirActividades />} />
+          </>
+        ) : (
+          <Route
+            path="/"
+            element={<LoginPage handleLogin={handleLogin} setAuthenticated={setAuthenticated} />}
+          />
+        )}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
