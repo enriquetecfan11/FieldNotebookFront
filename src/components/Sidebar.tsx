70cea72f5c35ba398e0c265a67ac5926ddc2e737
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-06-22 11:37:58 +0200

    updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -63,7 +63,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <NavLink to="/">
+        <NavLink to="/actividades">
           <img src={Logo} alt="Logo" className='mx-auto' />
           <h1 className="text-2xl font-semibold text-white text-center">Cuadeno de Campo</h1>
         </NavLink>
