61589bcb54299db1f4df3f7a0299ab3990bbb0ea
kikerodrivela <kikerodrivela@gmail.com>
2023-06-26 17:45:04 +0200

    Adde new layer

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -17,41 +17,41 @@ function MapContainer({ Coord_X, Coord_Y }) {
     // Capa base de Google Hybrid con opacidad de 0.5 (semi-transparente)
     const googleLayer = new TileLayer({
       source: new XYZ({
-        url: 'http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+        url: 'http://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}&hl=en',
       }),
-      opacity: 0.5, // Establece la opacidad a 0.5
+    opacity: 0.5, // Establece la opacidad a 0.5
     });
 
-    // Capa de GeoJSON => http://localhost:5000/Axarquia200.geojson
-    const geojsonLayer = new VectorLayer({
-      source: new VectorSource({
-        url: 'http://localhost:5000/map2.geojson',
-        format: new GeoJSON(),
-        featureProjection: 'EPSG:3857',
-      }),
-    });
+  // Capa de GeoJSON => http://localhost:5000/Axarquia200.geojson
+  const geojsonLayer = new VectorLayer({
+    source: new VectorSource({
+      url: 'http://localhost:5000/map2.geojson',
+      format: new GeoJSON(),
+      featureProjection: 'EPSG:3857',
+    }),
+  });
 
-    // Configuración del mapa
-    const map = new Map({
-      target: mapContainerRef.current,
-      layers: [googleLayer, geojsonLayer], // Cambia el orden de las capas si quieres que la capa de Google aparezca por encima
-      view: new View({
-        center: centerCoordinates, // Utiliza las coordenadas x e y proporcionadas
-        zoom: 17,
-      }),
-    });
+  // Configuración del mapa
+  const map = new Map({
+    target: mapContainerRef.current,
+    layers: [googleLayer, geojsonLayer], // Cambia el orden de las capas si quieres que la capa de Google aparezca por encima
+    view: new View({
+      center: centerCoordinates, // Utiliza las coordenadas x e y proporcionadas
+      zoom: 17,
+    }),
+  });
 
-    // Limpia el mapa y los eventos asociados al desmontar el componente
-    return () => {
-      map.setTarget(null);
-    };
-  }, [Coord_X, Coord_Y]);
+  // Limpia el mapa y los eventos asociados al desmontar el componente
+  return () => {
+    map.setTarget(null);
+  };
+}, [Coord_X, Coord_Y]);
 
-  return (
-    <div className="w-[450px] h-[300px]">
-      <div ref={mapContainerRef} className="w-full h-full" style={{ pointerEvents: 'none' }} />
-    </div>
-  );
+return (
+  <div className="w-[600px] h-[300px]">
+    <div ref={mapContainerRef} className="w-full h-full" style={{ pointerEvents: 'none' }} />
+  </div>
+);
 }
 
 export default MapContainer;
