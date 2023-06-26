b3e534801378b763e92e42db945a1141f7c01872
kikerodrivela <kikerodrivela@gmail.com>
2023-06-26 08:52:50 +0200

    minor fix

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://192.168.1.100:5000'; // Reemplaza esta URL con la base URL de tu API
+const API_BASE_URL = 'http://localhost:5000'; // Reemplaza esta URL con la base URL de tu API
 
 /*
   Actividades
